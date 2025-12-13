9ff4ea48b70271568c6f55f12248c87d922050d1
Mike <mike@example.com>
2025-12-12 20:43:02 -0800

    Debug: Add UI ping page & simplify build command

---
+++ src/app/ping-ui/page.tsx
@@ -0,0 +1,8 @@
+export default function PingPage() {
+    return (
+        <div style={{ padding: 50, fontFamily: 'sans-serif' }}>
+            <h1>Pong UI</h1>
+            <p>If you can see this, the Next.js App Router is working.</p>
+        </div>
+    );
+}
